b2566dabb6d3bc815777bf6649253b1ddd1e7750
eambutu <kip.phil@gmail.com>
2015-02-14 16:07:35 -0800

    Mentor help

--- app/src/main/java/com/example/morsetext/MorseCalculator.java
+++ app/src/main/java/com/example/morsetext/MorseCalculator.java
@@ -8,15 +8,22 @@ public class MorseCalculator {
                              's','t','u','v','w','x','y','z',',','.','!','?','#'};
     static char[] capitals = {' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
                                'S','T','U','V','W','X','Y','Z'};
-    public int charToInt(char a){
+    public boolean[] charToInt(char a){
         //used when reading texts, changing them into vibrations
+        //shows true if has a bit, false otherwise
+        //indexes 0,1,2,3,4 are the bits of the number in order
+        int temp = 0;
         for(int i=0;i<alphabet.length;i++){
-            if(a==alphabet[i]) return i;
+            if(a==alphabet[i]) temp=i;
         }
         for(int i=0;i<capitals.length;i++){
-            if(a==capitals[i]) return i;
+            if(a==capitals[i]) temp=i;
         }
-        return 0;
+        boolean[] bits = new boolean[5];
+        for(int i=0;i<5;i++){
+            bits[4-i] = temp%2==1?true:false;
+        }
+        return bits;
     }
     public char intToChar(int a){
         //used when taking in input, changing into text to send
